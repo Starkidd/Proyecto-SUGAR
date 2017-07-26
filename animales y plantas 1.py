4a446dc02570c90cec9fa6494af0dee237ad8c0b
starkidd <kiddstar47@gmail.com>
2017-07-26 17:39:18 -0600

    actualizacion del proyecto

--- animales y plantas 1.py
+++ animales y plantas 1.py
@@ -23,13 +23,13 @@ from gi.repository import Gtk, Gdk, GdkPixbuf
 
 #logger = logging.getLogger(__name__)
 
-class Empezar(Gtk.Window):
+class Inicio(Gtk.Window):
 	"""docstring for ClassName"""
 	def __init__(self, *args, **kwargs):
-		super(Empezar, self).__init__(*args,**kwargs)
+		super(Inicio, self).__init__(*args,**kwargs)
 		self.set_default_size(500,500)
 		self.agregar_contenedor()
-		self.agregar_acciones()
+		self.PrimeraVentanaPlanta()
 		
 
 	def agregar_contenedor(self):
@@ -40,7 +40,7 @@ class Empezar(Gtk.Window):
 		self.contenedor.set_column_homogeneous(True)
 		self.add(self.contenedor)
 
-	def agregar_acciones(self):
+	def PrimeraVentanaPlanta(self):
 
 		
 		self.planta = Gtk.Button('Clasificacion de las plantas')
@@ -51,8 +51,24 @@ class Empezar(Gtk.Window):
 		self.animales.connect('clicked',self.SegundaVentanaAnimal)
 		
 
+	def SegundaVentanaPlanta(self,btn):
 
-	def SegundaVentanaPlanta(self,b):
+		#self.gtk_window_set_title (title="hola")
+
+		for widget in self.contenedor:
+			self.contenedor.remove(widget)
+
+		self.boton_conflor_planta = Gtk.Button('Con Flor')
+		self.boton_sinFlor_planta = Gtk.Button('Sin Flor')
+		self.contenedor.attach(self.boton_conflor_planta,0,0,1,1)
+		self.contenedor.attach_next_to(self.boton_sinFlor_planta,self.boton_conflor_planta,Gtk.PositionType.RIGHT,1,1)
+
+		self.contenedor.show_all()
+		self.boton_conflor_planta.connect('clicked',self.TerceraVentanaPlanta)
+		self.boton_sinFlor_planta.connect('clicked',self.TerceraVentanaPlanta)
+
+
+	def TerceraVentanaPlanta(self,b):
 		
 		for widget in self.contenedor:
 			self.contenedor.remove(widget)
@@ -65,28 +81,38 @@ class Empezar(Gtk.Window):
 		self.contenedor.attach_next_to(self.numero_de_entrada_planta,self.label_numero_planta,Gtk.PositionType.RIGHT,1,1)
 		self.contenedor.attach_next_to(self.boton_inicio_planta,self.label_numero_planta,Gtk.PositionType.BOTTOM,1,1)
 		self.contenedor.show_all()
-		self.boton_inicio_planta.connect('clicked',self.TerceraVentanaPlanta)
-		
+		self.boton_inicio_planta.connect('clicked',self.CuartaVentanaPlanta)
 		
 
-
-	def TerceraVentanaPlanta(self,btn):
+	def CuartaVentanaPlanta(self,btn):
 
 		for widget in self.contenedor:
 			self.contenedor.remove(widget)
+
 		self.label_planta = Gtk.Label('las plantas de mi pais')
 		self.contenedor.attach(self.label_planta,0,0,1,1)
 		self.contenedor.show_all()
 
 
+	def SegundaVentanaAnimal(self,btn):
 
+		for widget in self.contenedor:
+			self.contenedor.remove(widget)
 
-	def SegundaVentanaAnimal(self,b):
+		self.boton_dondeviven_animal = Gtk.Button('Donde Viven')
+		self.boton_alimentacion_animal = Gtk.Button('Su alimentacion')
+		self.contenedor.attach(self.boton_dondeviven_animal,0,0,1,1)
+		self.contenedor.attach_next_to(self.boton_alimentacion_animal,self.boton_dondeviven_animal,Gtk.PositionType.RIGHT,1,1)
+		self.contenedor.show_all()
+		self.boton_dondeviven_animal.connect('clicked',self.TerceraVentanaAnimal)
+		self.boton_alimentacion_animal.connect('clicked',self.TerceraVentanaAnimal)
+		
+
+	def TerceraVentanaAnimal(self,b):
 		
 		for widget in self.contenedor:
 			self.contenedor.remove(widget)
 		
-
 		
 		self.boton_inicio_animal = Gtk.Button('Inicio')
 		self.label_numero_animal = Gtk.Label('Numero de veces')
@@ -95,12 +121,12 @@ class Empezar(Gtk.Window):
 		self.contenedor.attach_next_to(self.numero_de_entrada_animal,self.label_numero_animal,Gtk.PositionType.RIGHT,1,1)
 		self.contenedor.attach_next_to(self.boton_inicio_animal,self.label_numero_animal,Gtk.PositionType.BOTTOM,1,1)
 		self.contenedor.show_all()
-		self.boton_inicio_animal.connect('clicked',self.TerceraVentanaAnimal)
+		self.boton_inicio_animal.connect('clicked',self.CuartaVentanaAnimal)
 		
 		
 		
 
-	def TerceraVentanaAnimal(self,btn):
+	def CuartaVentanaAnimal(self,btn):
 		for widget in self.contenedor:
 			self.contenedor.remove(widget)
 
@@ -123,7 +149,7 @@ class Empezar(Gtk.Window):
 
 
 if __name__ == '__main__':
-	init = Empezar()
+	init = Inicio()
 	init.show_all()
 	Gtk.main()
 		
