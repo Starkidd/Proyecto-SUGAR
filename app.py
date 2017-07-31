aa73bcca993760c2db769010f0b7147b5e0eb578
starkidd <kiddstar47@gmail.com>
2017-07-31 16:52:07 -0600

    actualizacion de app.py con todas las ventanas necesarias

--- app.py
+++ app.py
@@ -10,13 +10,14 @@ from gi.repository import Gtk, Gdk, GdkPixbuf
 logger = logging.getLogger(__name__)
 
 
-class FaunaFlora(activity.Activity):
+class faunaflora(activity.Activity):
 
     def __init__(self, handle):
         activity.Activity.__init__(self, handle)
         self.agregar_toolbar()
         self.agregar_contenedor()
         self.PrimeraVentanaPlanta()
+        
 
     def agregar_toolbar(self):
         toolbar_box = ToolbarBox()
@@ -31,22 +32,99 @@ class FaunaFlora(activity.Activity):
         toolbar_box.show()
 
     def agregar_contenedor(self):
-        self.contenedor = Gtk.Grid()
-        self.contenedor.set_row_homogeneous(False)
-
-        self.contenedor.set_column_homogeneous(True)
-        self.add(self.contenedor)
+        self.canvas = Gtk.Grid()
+        self.add(self.canvas)
 
     def PrimeraVentanaPlanta(self):
         self.planta = Gtk.Button('Clasificacion de las plantas')
         self.animales = Gtk.Button('Clasificacion de los animales')
-        self.contenedor.attach(self.planta, 0, 0, 1, 1)
-        self.contenedor.attach_next_to(
+        self.canvas.attach(self.planta, 0, 0, 1, 1)
+        self.canvas.attach_next_to(
             self.animales,
             self.planta,
             Gtk.PositionType.RIGHT,
             1,
             1
         )
-        #self.planta.connect('clicked', self.SegundaVentanaPlanta)
-        #self.animales.connect('clicked', self.SegundaVentanaAnimal)
+        self.planta.connect('clicked',self.SegundaVentanaPlanta)
+        self.animales.connect('clicked',self.SegundaVentanaAnimal)
+        self.canvas.show_all()
+
+    def SegundaVentanaPlanta(self,btn):
+
+        for widget in self.canvas:
+            self.canvas.remove(widget)
+
+        self.boton_conflor_planta = Gtk.Button('Con Flor')
+        self.boton_sinFlor_planta = Gtk.Button('Sin Flor')
+        self.canvas.attach(self.boton_conflor_planta,0,0,1,1)
+        self.canvas.attach_next_to(self.boton_sinFlor_planta,self.boton_conflor_planta,Gtk.PositionType.RIGHT,1,1)
+
+        self.canvas.show_all()
+        self.boton_conflor_planta.connect('clicked',self.TerceraVentanaPlanta)
+        self.boton_sinFlor_planta.connect('clicked',self.TerceraVentanaPlanta)
+
+
+    def TerceraVentanaPlanta(self,b):
+        
+        for widget in self.canvas:
+            self.canvas.remove(widget)
+        
+    
+        self.boton_inicio_planta = Gtk.Button('Inicio')
+        self.label_numero_planta = Gtk.Label('Numero de veces')
+        self.numero_de_entrada_planta = Gtk.Entry() 
+        self.canvas.attach(self.label_numero_planta,0,0,1,1)
+        self.canvas.attach_next_to(self.numero_de_entrada_planta,self.label_numero_planta,Gtk.PositionType.RIGHT,1,1)
+        self.canvas.attach_next_to(self.boton_inicio_planta,self.label_numero_planta,Gtk.PositionType.BOTTOM,1,1)
+        self.canvas.show_all()
+        self.boton_inicio_planta.connect('clicked',self.CuartaVentanaPlanta)
+
+
+    def CuartaVentanaPlanta(self,btn):
+
+        for widget in self.canvas:
+            self.canvas.remove(widget)
+
+        self.label_planta = Gtk.Label('las plantas de mi pais')
+        self.canvas.attach(self.label_planta,0,0,1,1)
+        self.canvas.show_all()
+
+
+    def SegundaVentanaAnimal(self,btn):
+
+        for widget in self.canvas:
+            self.canvas.remove(widget)
+
+        self.boton_dondeviven_animal = Gtk.Button('Donde Viven')
+        self.boton_alimentacion_animal = Gtk.Button('Su alimentacion')
+        self.canvas.attach(self.boton_dondeviven_animal,0,0,1,1)
+        self.canvas.attach_next_to(self.boton_alimentacion_animal,self.boton_dondeviven_animal,Gtk.PositionType.RIGHT,1,1)
+        self.canvas.show_all()
+        self.boton_dondeviven_animal.connect('clicked',self.TerceraVentanaAnimal)
+        self.boton_alimentacion_animal.connect('clicked',self.TerceraVentanaAnimal)
+
+
+    def TerceraVentanaAnimal(self,b):
+        
+        for widget in self.canvas:
+            self.canvas.remove(widget)
+        
+        
+        self.boton_inicio_animal = Gtk.Button('Inicio')
+        self.label_numero_animal = Gtk.Label('Numero de veces')
+        self.numero_de_entrada_animal = Gtk.Entry() 
+        self.canvas.attach(self.label_numero_animal,0,0,1,1)
+        self.canvas.attach_next_to(self.numero_de_entrada_animal,self.label_numero_animal,Gtk.PositionType.RIGHT,1,1)
+        self.canvas.attach_next_to(self.boton_inicio_animal,self.label_numero_animal,Gtk.PositionType.BOTTOM,1,1)
+        self.canvas.show_all()
+        self.boton_inicio_animal.connect('clicked',self.CuartaVentanaAnimal)
+
+
+    def CuartaVentanaAnimal(self,btn):
+        for widget in self.canvas:
+            self.canvas.remove(widget)
+
+        self.label_animal = Gtk.Label('Los animales de mi pais')
+        self.canvas.attach(self.label_animal,0,0,1,1)
+        self.canvas.show_all()
